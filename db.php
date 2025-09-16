0850ec218be6fc035ce3746fc1b78e15000bfafa
sivalokeshp <sivalokesh4367@gmail.com>
2025-09-16 16:59:40 +0530

    first commit

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 // db.php - Database connection
 $DB_HOST = 'localhost';
 $DB_USER = 'root';      // change if necessary
-$DB_PASS = 'PSL2026';          // change if necessary
+$DB_PASS = '';          // change if necessary
 $DB_NAME = 'student_db';
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
